9bb77eb65ee8c78fc7ca97688d9d19b78d939e65
Björn Hjortsten <bjorn.hjortsten@kaigan.se>
2014-09-30 13:03:22 +0200

    Added the ability to cache searches.

--- Controller/SearchController.php
+++ Controller/SearchController.php
@@ -19,13 +19,30 @@ class SearchController extends ControllerAbstract {
 	/**
 	 * Search for Media in QBank
 	 * @param Search $search Search parameters
+	 * @param QBankCachePolicy $cachePolicy Leaving cachePolicy null will use the default cache policy
 	 * 
 	 * @return SearchResult
 	 */
-	public function search($search) {
+	public function search($search, QBankCachePolicy $cachePolicy = null) {
+		$cachePolicy = ($cachePolicy !== null) ? $cachePolicy : $this->cachePolicy;
+		if ($cachePolicy->isEnabled() && $this->cache->contains(md5('search-'.json_encode($search)))) {
+			$this->logger->info(
+				'Using cached search response. POST /search',
+				array(
+					'endpoint' => '/search',
+					'parameters' => json_encode($search),
+					'method' => 'POST'
+				)
+			);
+			return new SearchResult($this->cache->fetch(md5('search-'.json_encode($search))));
+		}
 		$query = array();
 		$query = array_merge($query, $search->jsonSerialize());
-		return new SearchResult($this->post('v1/search', $query));
+		$response = $this->post('v1/search', $query);
+		if ($cachePolicy->isEnabled()) {
+			$this->cache->save(md5('search-'.json_encode($search)), $response, $cachePolicy->getLifetime());
+		}
+		return new SearchResult($response);
 	}
 
 }
