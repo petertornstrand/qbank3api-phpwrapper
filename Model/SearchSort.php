d74d5786d4e2b3f2cbc4ce9f1a17b1daf2b16eba
Björn Hjortsten <bjorn.hjortsten@qbank.se>
2015-02-02 13:56:35 +0100

    Added support for requesting sorted search results.

--- Model/SearchSort.php
+++ Model/SearchSort.php
@@ -13,7 +13,19 @@ namespace QBNK\QBank\API\Model;
 
 class SearchSort implements \JsonSerializable  {
 
+	const SORT_ID = 'id';
+	const SORT_NAME = 'name';
+	const SORT_CREATED = 'created';
+	const SORT_UPDATED = 'updated';
+	const SORT_RELEVANCE = 'relevance';
+	const SORT_CATEGORY = 'category';
+	const SORT_SIZE = 'size';
+	const SORT_MIMETYPE = 'mimetype';
+	const SORT_POPULARITY = 'popularity';
+	const SORT_PROPERTY = 'property';
 
+	const SORT_DIRECTION_ASCENDING = 'asc';
+	const SORT_DIRECTION_DESCENDING = 'desc';
 
 	/**
 	 * Any of the SORT_* constants
@@ -27,6 +39,23 @@ class SearchSort implements \JsonSerializable  {
 	 */
 	protected $sortDirection;
 
+	/**
+	 * When sorting on Media Popularity, the source to check (QBank Backend, frontend, etc)
+	 * @var int $sourceId
+	 */
+	protected $sourceId;
+
+	/**
+	 * When sorting on Media Popularity, a optional dateRange to find popular media within
+	 * @var DateTimeRange $dateRange
+	 */
+	protected $dateRange;
+
+	/**
+	 * When sorting on a property, the system name of the property to sort on
+	 * @var string $systemName
+	 */
+	protected $systemName;
 
 	/**
 	 * Constructs a {@link SearchSort }.
@@ -45,6 +74,20 @@ class SearchSort implements \JsonSerializable  {
 	
 		if (isset($parameters['sortDirection'])) {
 			$this->setSortDirection($parameters['sortDirection']);
+		} else {
+			$this->sortDirection = self::SORT_DIRECTION_DESCENDING;
+		}
+
+		if (isset($parameters['sourceId'])) {
+			$this->setSourceId($parameters['sourceId']);
+		}
+
+		if (isset($parameters['dateRange'])) {
+			$this->setDateRange($parameters['dateRange']);
+		}
+
+		if (isset($parameters['systemName'])) {
+			$this->setSystemName($parameters['systemName']);
 		}
 	
 	}
@@ -66,7 +109,17 @@ class SearchSort implements \JsonSerializable  {
 		return $this->sortDirection;
 	}
 
+	public function getSourceId() {
+		return $this->sourceId;
+	}
 
+	public function getDateRange() {
+		return $this->dateRange;
+	}
+
+	public function getSystemName() {
+		return $this->systemName;
+	}
 
 	/**
 	 * Sets the "sortField" of the SearchSort
@@ -88,6 +141,28 @@ class SearchSort implements \JsonSerializable  {
 		return $this;
 	}
 
+	public function setSourceId($sourceId) {
+		$this->sourceId = (int)$sourceId;
+		return $this;
+	}
+
+	public function setDateRange($dateRange) {
+		if ($dateRange instanceof DateTimeRange) {
+			$this->dateRange = $dateRange;
+		} else if (is_array($dateRange)) {
+			$this->dateRange = new DateTimeRange($dateRange);
+		} else {
+			$this->dateRange = null;
+			trigger_error('Argument must be an object of class DateTimeRange. Data loss!', E_USER_WARNING);
+		}
+		return $this;
+	}
+
+	public function setSystemName($systemName) {
+		$this->systemName = $systemName;
+		return $this;
+	}
+
 
 	/**
 	 * Gets all data that should be available in a json representation.
@@ -101,6 +176,15 @@ class SearchSort implements \JsonSerializable  {
 		}
 		if ($this->sortDirection !== null) {
 			$array['sortDirection'] = $this->sortDirection;
+		}
+		if ($this->sourceId !== null) {
+			$array['sourceId'] = $this->sourceId;
+		}
+		if ($this->dateRange !== null) {
+			$array['dateRange'] = $this->dateRange;
+		}
+		if ($this->systemName !== null) {
+			$array['systemName'] = $this->systemName;
 		}
 		return $array;
 	}
