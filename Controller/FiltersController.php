a7d76e3eff9d1b189cbda11a4626b073d86e4622
Fredrik Sundblom <fredrik.sundblom@qbank.se>
2017-05-06 21:46:07 +0200

    - Added option to freetext-filter: It now takes a whole string and applies desired and/or logic between each word. - Also fixed the indenting…

--- Controller/FiltersController.php
+++ Controller/FiltersController.php
@@ -3,123 +3,127 @@
 namespace QBNK\QBank\API\Controller;
 
 use QBNK\QBank\API\CachePolicy;
-    use QBNK\QBank\API\Model\FilterItem;
+use QBNK\QBank\API\Model\FilterItem;
 
-    class FiltersController extends ControllerAbstract
-    {
-        /**
-     * Returns a array of FilterItem for the chosen categories.
-     * 
-     * , optionally filtered by specific DeploymentSites.
-     * 
-     * @param string $categoryIds Comma separated string categoryIds we should fetch mediaIds for.
-     * @param string $deploymentSiteIds Comma separated string of deploymentSiteIds we should fetch mediaIds for.
-     * @param CachePolicy $cachePolicy A custom cache policy used for this request only.
-     
-     * @return FilterItem[]	 
-     */
-    public function categories($categoryIds, $deploymentSiteIds = null, CachePolicy $cachePolicy = null)
-    {
-        $parameters = [
-            'query'   => ['deploymentSiteIds' => $deploymentSiteIds],
-            'body'    => json_encode([]),
-            'headers' => [],
-        ];
-        $result = $this->get('v1/filters/categories/'.$categoryIds.'', $parameters, $cachePolicy);
-        foreach ($result as &$entry) {
-            $entry = new FilterItem($entry);
-        }
-        unset($entry);
-        reset($result);
+class FiltersController extends ControllerAbstract
+{
+	const FREETEXT_AND = 'AND';
+	const FREETEXT_OR  = 'OR';
 
-        return $result;
-    }
+	/**
+	 * Returns a array of FilterItem for the chosen categories.
+	 *
+	 * , optionally filtered by specific DeploymentSites.
+	 *
+	 * @param string $categoryIds Comma separated string categoryIds we should fetch mediaIds for.
+	 * @param string $deploymentSiteIds Comma separated string of deploymentSiteIds we should fetch mediaIds for.
+	 * @param CachePolicy $cachePolicy A custom cache policy used for this request only.
+	 *
+	 * @return FilterItem[]
+	 */
+	public function categories($categoryIds, $deploymentSiteIds = null, CachePolicy $cachePolicy = null)
+	{
+		$parameters = [
+			'query'   => ['deploymentSiteIds' => $deploymentSiteIds],
+			'body'    => json_encode([]),
+			'headers' => [],
+		];
+		$result = $this->get('v1/filters/categories/'.$categoryIds.'', $parameters, $cachePolicy);
+		foreach ($result as &$entry) {
+			$entry = new FilterItem($entry);
+		}
+		unset($entry);
+		reset($result);
 
-    /**
-     * Returns a array of FilterItem for the chosen folder subfolders.
-     * 
-     * , optionally filtered by specific CategoryIds and/or DeploymentSites.
-     * 
-     * @param int $parentFolderId The folder id..
-     * @param string $categoryIds Comma separated string categoryIds we should fetch mediaIds for.
-     * @param string $deploymentSiteIds Comma separated string of deploymentSiteIds we should fetch mediaIds for.
-     * @param CachePolicy $cachePolicy A custom cache policy used for this request only.
-     
-     * @return FilterItem[]	 
-     */
-    public function folder($parentFolderId, $categoryIds = null, $deploymentSiteIds = null, CachePolicy $cachePolicy = null)
-    {
-        $parameters = [
-            'query'   => ['categoryIds' => $categoryIds, 'deploymentSiteIds' => $deploymentSiteIds],
-            'body'    => json_encode([]),
-            'headers' => [],
-        ];
-        $result = $this->get('v1/filters/folder/'.$parentFolderId.'', $parameters, $cachePolicy);
-        foreach ($result as &$entry) {
-            $entry = new FilterItem($entry);
-        }
-        unset($entry);
-        reset($result);
+		return $result;
+	}
 
-        return $result;
-    }
+	/**
+	 * Returns a array of FilterItem for the chosen folder subfolders.
+	 *
+	 * , optionally filtered by specific CategoryIds and/or DeploymentSites.
+	 *
+	 * @param int $parentFolderId The folder id..
+	 * @param string $categoryIds Comma separated string categoryIds we should fetch mediaIds for.
+	 * @param string $deploymentSiteIds Comma separated string of deploymentSiteIds we should fetch mediaIds for.
+	 * @param CachePolicy $cachePolicy A custom cache policy used for this request only.
+	 *
+	 * @return FilterItem[]
+	 */
+	public function folder($parentFolderId, $categoryIds = null, $deploymentSiteIds = null, CachePolicy $cachePolicy = null)
+	{
+		$parameters = [
+			'query'   => ['categoryIds' => $categoryIds, 'deploymentSiteIds' => $deploymentSiteIds],
+			'body'    => json_encode([]),
+			'headers' => [],
+		];
+		$result = $this->get('v1/filters/folder/'.$parentFolderId.'', $parameters, $cachePolicy);
+		foreach ($result as &$entry) {
+			$entry = new FilterItem($entry);
+		}
+		unset($entry);
+		reset($result);
 
-    /**
-     * Returns a array of FilterItem for a specific freetext.
-     * 
-     * , optionally filtered by DeploymentSites.
-     * 
-     * @param string $freetext String to filter by.
-     * @param string $deploymentSiteIds Comma separated string of deploymentSiteIds we should fetch mediaIds for.
-     * @param CachePolicy $cachePolicy A custom cache policy used for this request only.
-     
-     * @return FilterItem[]	 
-     */
-    public function freetext($freetext, $deploymentSiteIds = null, CachePolicy $cachePolicy = null)
-    {
-        $parameters = [
-            'query'   => ['deploymentSiteIds' => $deploymentSiteIds],
-            'body'    => json_encode([]),
-            'headers' => [],
-        ];
-        $result = $this->get('v1/filters/freetext/'.$freetext.'', $parameters, $cachePolicy);
-        foreach ($result as &$entry) {
-            $entry = new FilterItem($entry);
-        }
-        unset($entry);
-        reset($result);
+		return $result;
+	}
 
-        return $result;
-    }
+	/**
+	 * Returns a array of FilterItem for a specific freetext.
+	 *
+	 * , optionally filtered by DeploymentSites.
+	 *
+	 * @param string $freetext String to filter by.
+	 * @param string $deploymentSiteIds Comma separated string of deploymentSiteIds we should fetch mediaIds for.
+	 * @param string $mode The method (AND|OR) to filter words by.
+	 * @param CachePolicy $cachePolicy A custom cache policy used for this request only.
+	 *
+	 * @return FilterItem[]
+	 */
+	public function freetext($freetext, $deploymentSiteIds = null, $mode = self::FREETEXT_OR, CachePolicy $cachePolicy = null)
+	{
+		$parameters = [
+			'query'   => ['deploymentSiteIds' => $deploymentSiteIds, 'mode' => $mode],
+			'body'    => json_encode([]),
+			'headers' => [],
+		];
+		$result = $this->get('v1/filters/freetext/'.$freetext.'', $parameters, $cachePolicy);
+		foreach ($result as &$entry) {
+			$entry = new FilterItem($entry);
+		}
+		unset($entry);
+		reset($result);
 
-    /**
-     * Returns a array of FilterItem for the chosen property.
-     * 
-     * , optionally filtered by specific CategoryIds and/or DeploymentSites.
-     * 
-     * @param string $systemName System name of property to filter by.
-     * @param bool $preloadNames If item names should be preloaded from property type.
-     * @param string $categoryIds Comma separated string categoryIds we should fetch mediaIds for.
-     * @param string $deploymentSiteIds Comma separated string of deploymentSiteIds we should fetch mediaIds for.
-     * @param bool $isHierarchical 
-     * @param CachePolicy $cachePolicy A custom cache policy used for this request only.
-     
-     * @return FilterItem[]	 
-     */
-    public function property($systemName, $preloadNames = false, $categoryIds = null, $deploymentSiteIds = null, $isHierarchical = false, CachePolicy $cachePolicy = null)
-    {
-        $parameters = [
-            'query'   => ['preloadNames' => $preloadNames, 'categoryIds' => $categoryIds, 'deploymentSiteIds' => $deploymentSiteIds, 'isHierarchical' => $isHierarchical],
-            'body'    => json_encode([]),
-            'headers' => [],
-        ];
-        $result = $this->get('v1/filters/property/'.$systemName.'', $parameters, $cachePolicy);
-        foreach ($result as &$entry) {
-            $entry = new FilterItem($entry);
-        }
-        unset($entry);
-        reset($result);
+		return $result;
+	}
 
-        return $result;
-    }
-    }
+	/**
+	 * Returns a array of FilterItem for the chosen property.
+	 *
+	 * , optionally filtered by specific CategoryIds and/or DeploymentSites.
+	 *
+	 * @param string $systemName System name of property to filter by.
+	 * @param bool $preloadNames If item names should be preloaded from property type.
+	 * @param string $categoryIds Comma separated string categoryIds we should fetch mediaIds for.
+	 * @param string $deploymentSiteIds Comma separated string of deploymentSiteIds we should fetch mediaIds for.
+	 * @param bool $isHierarchical
+	 * @param CachePolicy $cachePolicy A custom cache policy used for this request only.
+	 *
+	 * @return FilterItem[]
+	 */
+	public function property($systemName, $preloadNames = false, $categoryIds = null, $deploymentSiteIds = null, $isHierarchical = false, CachePolicy $cachePolicy = null)
+	{
+		$parameters = [
+			'query'   => ['preloadNames' => $preloadNames, 'categoryIds' => $categoryIds, 'deploymentSiteIds' => $deploymentSiteIds, 'isHierarchical' => $isHierarchical],
+			'body'    => json_encode([]),
+			'headers' => [],
+		];
+		$result = $this->get('v1/filters/property/'.$systemName.'', $parameters, $cachePolicy);
+		foreach ($result as &$entry) {
+			$entry = new FilterItem($entry);
+		}
+		unset($entry);
+		reset($result);
+
+		return $result;
+	}
+}
